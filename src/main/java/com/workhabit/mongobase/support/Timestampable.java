625a28e986bfdd66a8fbd2bfdea0a739352b74a2
Kenzi Stewart <4248+pixelcat@users.noreply.github.com>
2026-02-02 11:57:18 -0800

    Update Timestampable.java

--- src/main/java/com/workhabit/mongobase/support/Timestampable.java
+++ src/main/java/com/workhabit/mongobase/support/Timestampable.java
@@ -3,7 +3,7 @@ package com.workhabit.mongobase.support;
 import org.joda.time.DateTime;
 
 /**
- * Copyright 2014 - Aaron Stewart
+ * Copyright 2014 - Kenzi Stewart
  * Date: 4/20/14, 6:53 PM
  */
 public interface Timestampable
